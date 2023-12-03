632f22ca6d6bd3791848b5af5bea2cd2a8b80360
Yarema Kertytsky <y.kertytsky@gmail.com>
2023-12-03 13:11:56 +0200

    initital setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+// Bootstrap
+import Button from 'react-bootstrap/Button'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello, world!</h1>
+    </>
+  )
+}
+
+export default App
